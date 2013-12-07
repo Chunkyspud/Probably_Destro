ae414f7ea358d3b6ad1ec9c84c2c32d0e43ed59a
Chunkyspud <lundeenjordan@yahoo.com>
2013-12-07 14:09:32 -0600

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -22,21 +22,23 @@ function destro.Healthstone(target)
   return true
 end
 
-function destro.CurseofElements(target)
+function destro.Boss(target)
   if UnitLevel("target") == -1 then 
     return true 
     end
   return false
 end
 
-function destro.Doomguard (target)
+function destro.Doomguard(unit)
   if not (UnitBuff("player", 2825) or
-     UnitBuff("player", 90355) or
-     UnitBuff("player", 80353) or
-     UnitBuff("player", 32182)) then
-   return false
- end
- return true
+			UnitBuff("player", 32182) or 
+			UnitBuff("player", 80353) or
+			UnitBuff("player", 90355)) then
+		return false
+	end
+	if not ProbablyEngine.condition["modifier.cooldowns"] then return false end
+	if UnitLevel(target) ~= -1 then return false end
+	return true 
 end
 
 ProbablyEngine.library.register("destro", destro)
